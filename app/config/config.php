8a0079c9bee0702d5d366fa080ae1292a99eebcd
waghtot <waghtot@gmail.com>
2021-05-09 17:36:33 +0100

    update

--- app/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('DB_HOST', 'localhost');
-define('DB_USER', 'admin');
-define('DB_PASS', 'Klamka22k!');
-define('DB_NAME', 'MM_CORE');
-
-?>
-
--- app/config/constans.php
+++
@@ -1,6 +0,0 @@
-<?php
-/*
-define('SIGNATURE', parse_ini_file('/etc/config/config.ini')['key']);
-define('PREFIX', 'http://api-');
-define('DNS', '.dev.com');
-*/
